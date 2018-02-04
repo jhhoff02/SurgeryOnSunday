56584960e57ca225c8101c4df206cdefe40c9d3f
Kari L Dunn <kdunn0909@gmail.com>
2018-02-04 17:15:41 -0500

    New commit

---
+++ SurgeryOnSunday/SurgeryOnSunday/Controllers/VolunteerFormController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SurgeryOnSunday.Controllers
+{
+    public class VolunteerFormController : Controller
+    {
+        // GET: VolunteerForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PhysVolForm()
+        {
+            return View();
+        }
+
+        public ActionResult NonPhysClinicalVolForm()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralForm()
+        {
+            return View();
+        }
+
+        public ActionResult MedicalInterpreterForm()
+        {
+            return View();
+        }
+
+        public ActionResult VolunteerCommentForm()
+        {
+            return View();
+        }
+
+    }
+}
+    
